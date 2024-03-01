736d02b10aa8353b6e2174c3b6a81970b36e20aa
madelyndm <madelyn.mcgurk@circuitrunners.com>
2024-03-01 17:56:56 -0500

    fixing errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outreachbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outreachbot.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+
 @TeleOp
 public class outreachbot extends LinearOpMode {
     @Override
@@ -17,16 +18,17 @@ public class outreachbot extends LinearOpMode {
         Motor4 = hardwareMap.get(DcMotor.class, "Motor4");
 
         //initializing hardware
-        watiForStart();
+        waitForStart();
         if (isStopRequested()) return;
         while (opModeIsActive()) {
 
             //reading gamepad input
             Motor1.setPower(gamepad1.left_stick_y + gamepad1.right_stick_x);
-            Motor2.setPower(gamepad1.left_stick_y) + gamepad1.right_stick_x);
+            Motor2.setPower(gamepad1.left_stick_y + gamepad1.right_stick_x);
             Motor3.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x);
             Motor4.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x);
-            
 
-            
+
         }
+    }
+}
